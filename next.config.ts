be6d4eab0d71aaf6afd707529ec074e99ae2558a
Traxle Developer <admin@traxleapp.com>
2026-04-20 00:09:03 +0300

    TraxleAPP Ekosistem Guncellemesi

--- next.config.ts
+++ next.config.ts
@@ -8,10 +8,11 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        // Şubelerin göreceği havalı link:
-        source: 'https://www.traxleapp.com/guncelleme/KunyeX_Master_Client.exe',
-        // Firebase'den kopyaladığın o uzun linki BURAYA yapıştır:
-        destination: 'gs://kunyex-lisans.firebasestorage.app/KunyeX_Master_Client.exe',
+        // 1. DÜZELTME: Sadece klasör yolu yazılır (Domain kullanılmaz)
+        source: '/guncelleme/KunyeX_Master_Client.exe',
+        
+        // 2. DÜZELTME: Firebase'den aldığın HTTPS indirme linkini buraya yapıştır
+        destination: 'https://firebasestorage.googleapis.com/v0/b/kunyex-lisans.firebasestorage.app/o/KunyeX_Master_Client.exe?alt=media&token=8bd0e195-403c-4fe1-9975-d39aa5ad229b',
       },
     ];
   },
