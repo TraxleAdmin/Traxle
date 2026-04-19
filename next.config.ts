73950fac328cf044f4084108062c5e0723d494a5
Traxle Developer <admin@traxleapp.com>
2026-04-20 00:03:23 +0300

    TraxleAPP Ekosistem Guncellemesi

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 🔥 BU SATIR EKLENDİ: iyzipay hatasını çözer
+  // 🔥 Mevcut iyzipay ayarın korunuyor
   serverExternalPackages: ["iyzipay"],
+
+  // 🔥 GÖRÜNMEZ KÖPRÜ (Rewrite Maskesi)
+  async rewrites() {
+    return [
+      {
+        // Şubelerin göreceği havalı link:
+        source: 'https://www.traxleapp.com/guncelleme/KunyeX_Master_Client.exe',
+        // Firebase'den kopyaladığın o uzun linki BURAYA yapıştır:
+        destination: 'gs://kunyex-lisans.firebasestorage.app/KunyeX_Master_Client.exe',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
